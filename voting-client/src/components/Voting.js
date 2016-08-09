1e26d3574e4e91db94eb0e29345a1a8501df98c9
Joe Averbukh <joeaverbukh@gmail.com>
2016-08-09 10:20:43 -0700

    Add voting screen

--- voting-client/src/components/Voting.js
+++ voting-client/src/components/Voting.js
@@ -1,19 +1,59 @@
 import React, {Component}  from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class Voting extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.winner ?
+          <Winner ref="winner" winner={this.props.winner} /> :
+          <Vote {...this.props} />}
+      </div>
+    )
+  }
+};
+
+class Vote extends Component {
+
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate =
+      PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
   getPair = () => {
     return this.props.pair || [];
+  };
+
+  hasVotedFor = (entry) => {
+    return entry === this.props.hasVoted;
+  }
+
+  isDisabled = () => {
+    return !!this.props.hasVoted;
   };
 
   render() {
     return (
       <div>
         {this.getPair().map(entry =>
-          <button key={entry}>
+          <button
+            key={entry}
+            disabled={this.isDisabled()}
+            onClick={() => this.props.vote(entry)}>
             <h1>{entry}</h1>
+            {this.hasVotedFor(entry) ?
+              <div className="label">Voted</div> :
+              null}
           </button>
           )}
       </div>
     );
   }
 }
+
+const Winner = ({winner}) => (
+  <div className="winner">
+    Winner is {winner}!
+  </div>
+);
---
+++ voting-client/test/components/Voting_spec.js
@@ -0,0 +1,85 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag,
+  Simulate,
+} from 'react-addons-test-utils';
+
+import Voting from '../../src/components/Voting';
+
+describe('Voting', () => {
+
+  it('renders a pair of buttons', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]} />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].textContent).to.equal('Trainspotting');
+    expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              vote={vote}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
+  });
+
+  it('renders as a pure component', () => {
+    // Notice this is const
+    const pair = ['Trainspotting', '28 Days Later'];
+
+    const container = document.createElement('div');
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    // Const can be modified but not re-assigned
+    pair[0] = 'Sunshine';
+    component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    );
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+
+    // Should be true since pure component does not do deep equality check
+    expect(firstButton.textContent).to.equal('Trainspotting');
+  });
+
+  it('does update DOM when prop changes', () => {
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const container = document.createElement('div');
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    const newPair = pair.set(0, 'Sunshine');
+    component = ReactDOM.render(
+      <Voting pair={newPair} />,
+      container
+    );
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Sunshine');
+  });
+
+});
